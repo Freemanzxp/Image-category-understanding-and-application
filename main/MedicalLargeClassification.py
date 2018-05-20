d8d3cc866fb869f6e45be60ce1f03d85523b5b4a
zp <972475683@qq.com>
2018-05-20 22:54:47 +0800

    main

---
+++ main/MedicalLargeClassification.py
@@ -0,0 +1,120 @@
+"""
+Created on ：
+@author: Freeman
+"""
+import numpy as np
+from tkinter import *
+import tkinter.filedialog
+from keras import backend as K
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+
+
+def getLargeFileNames():
+    filenames = tkinter.filedialog.askopenfilenames()
+    if len(filenames) != 0:
+        rightCategroy = ""
+        string_filename = ""
+        count = 0
+        for i in range(0, len(filenames)):
+            count += 1
+            string_filename += str(filenames[i]) + "\n"
+            temp = str(filenames[i]).split('/')[-1].split('.')[0]
+            if count % 5 == 0:
+                rightCategroy += temp + "\n"
+            else:
+                rightCategroy += temp + " "
+        LargePictureNames.set(string_filename)
+        lb1.config(text="图片真实类别：\n" + rightCategroy)
+    else:
+        LargePictureNames.set('')
+        lb1.config(text="您没有选择任何图片")
+
+
+def Large_model_predict():
+    if LargePictureNames.get() != '':
+        picList = LargePictureNames.get().split('\n')[:-1]
+        data = []
+        for i in picList:
+            img = load_img(i, target_size=(img_width, img_height))
+            x = img_to_array(img)
+            data.append(x)
+        data = np.stack(data,axis=0)
+        data = preprocess_input(data)  # 对样本执行-逐样本均值消减-的归一化
+        model1 = load_model('MedicalLargeClassificationModel_weights_15.h5')
+        result1 = model1.predict(data)
+        K.clear_session()
+
+        model2 = load_model('MedicalSegmentClassificationModel_weights_15.h5')
+        result2 = model2.predict(data)
+        K.clear_session()
+
+        number = result1.shape[0]
+        strs = ''
+        count = 0
+        for i in range(number):
+            count += 1
+            res1 = np.max(result1[i])
+            categroy = categroyDict1[np.where(result1[i] == res1)[0][0]]
+            if count % 5 == 0:
+                if categroy == '医学':
+                    res2 = np.max(result2[i])
+                    segment = categroyDict2[np.where(result2[i] == res2)[0][0]]
+                    strs += str(categroy) + '-' + str(segment) + "\n"
+                else:
+                    strs += str(categroy)+ "\n"
+
+            else:
+                if categroy == '医学':
+                    res2 = np.max(result2[i])
+                    segment = categroyDict2[np.where(result2[i] == res2)[0][0]]
+                    strs += str(categroy) + '-' + str(segment) + " "
+                else:
+                    strs += str(categroy) + " "
+        lb3.config(text="预测类别：\n" + strs)
+    else:
+        lb3.config(text="图片选择有误！")
+
+
+if __name__ == "__main__":
+
+    # 参数初始化
+    img_width, img_height = 224, 224
+
+    # 类别字典
+    categroyDict1 ={0:'   动物   ',1:'   室内   ',2:'医学',3:'   人物   ',4:'   交通   '}
+    categroyDict2 ={0:'胸部',1:'头部',2:'四肢'}
+
+    # GUI
+    root = tkinter.Tk()
+    root.geometry('700x550+300+50')
+
+    LargePictureNames = StringVar()
+    SegmentPictureNames = StringVar()
+
+    root.title('Image recognition system          Copyright © 2018 by Freeman')
+    root['bg'] = '#a9ed96'
+    root.attributes("-alpha",0.97)
+
+
+    lb0 = Label(root, text='图像识别系统', width=20, font = 'Helvetica -20 bold',bg='#a9ed96',height=3)
+    lb0.grid(row=0, column=0, stick=W, pady=1,)
+
+    lb5 = Label(root, text='注：若为医学类图像暂时仅支持胸部、头部、四肢识别',bg='#a9ed96', fg='red')
+    lb5.grid(row=0, column=1, stick=W)
+
+    btn1 = Button(root, text="选择需要预测的图片", width=20, command=getLargeFileNames)
+    btn1.grid(row=1, column=0, stick=W, pady=10, padx=50)
+
+    lb1 = Label(root, text='', width=60)
+    lb1.grid(row=1, column=1, pady=20)
+
+    btn3 = Button(root, text="预 测", width=20,  bg='SkyBlue',command=Large_model_predict)
+    btn3.grid(row=4, column=0, stick=W,  pady=10, padx=50)
+
+    lb3 = Label(root, text='', width=60)
+    lb3.grid(row=4, column=1, pady=30)
+
+
+    root.mainloop()
---
+++ main/MedicalLargeFine_tuning.py
@@ -0,0 +1,132 @@
+"""
+Created on ：26/1/2018
+@author: Freeman
+"""
+from keras.callbacks import Callback
+from keras.applications import VGG16
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.layers import Dense
+from keras.optimizers import SGD
+import matplotlib.pyplot as plt
+
+
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = {'batch':[], 'epoch':[]}
+        self.accuracy = {'batch':[], 'epoch':[]}
+        self.val_loss = {'batch':[], 'epoch':[]}
+        self.val_acc = {'batch':[], 'epoch':[]}
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses['batch'].append(logs.get('loss'))
+        self.accuracy['batch'].append(logs.get('acc'))
+        self.val_loss['batch'].append(logs.get('val_loss'))
+        self.val_acc['batch'].append(logs.get('val_acc'))
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses['epoch'].append(logs.get('loss'))
+        self.accuracy['epoch'].append(logs.get('acc'))
+        self.val_loss['epoch'].append(logs.get('val_loss'))
+        self.val_acc['epoch'].append(logs.get('val_acc'))
+
+    def loss_plot(self, loss_type):
+        iters = range(len(self.losses[loss_type]))
+        plt.figure()
+        # acc
+        plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
+        # loss
+        plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+        if loss_type == 'epoch':
+            # val_acc
+            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
+            # val_loss
+            plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+        plt.grid(True)
+        plt.xlabel(loss_type)
+        plt.ylabel('acc-loss')
+        plt.legend(loc="upper right")
+        plt.savefig("loss-acc/epochs_15.png")
+        plt.show()
+
+
+# build the VGG16 network
+def vgg16_model(img_rows, img_cols, num_classes=5):
+    model = VGG16(weights='imagenet', include_top=True)
+    # for i in model.layers:
+    #     print(i.name)
+    #     print(i.get_weights())
+    # print(model.get_layer('block4_conv3').get_weights())
+    # model.summary()
+    model.layers.pop()
+    model.layers.pop()
+    model.layers.pop()
+    model.outputs = [model.layers[-1].output]
+    x=Dense(1024, activation='relu')(model.layers[-1].output)
+    x=Dense(128, activation='relu')(x)
+    x=Dense(num_classes, activation='softmax')(x)
+    model=Model(model.input,x)
+    for layer in model.layers[:15]:
+        layer.trainable = False
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    return model
+
+def train_save_model():
+    # 图片生成器:用以生成一个batch的图像数据，
+    # 支持实时数据提升。训练时该函数会无限生成数据，直到达到规定的epoch次数为止。
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    # fine-tune the model
+    model = vgg16_model(224, 224, 5)
+
+    # loss-history
+    history = LossHistory()
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=16,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=16,
+        callbacks=[history]
+    )
+
+    loss, acc = model.evaluate_generator(
+        validation_generator,
+        steps=8)
+
+    print("loss:", loss, "acc:", acc)
+    history.loss_plot('epoch')
+    # 保存model
+    model.save('MedicalLargeClassificationModel_weights_15.h5')
+
+
+if __name__ == "__main__":
+    # 参数初始化
+    img_width, img_height = 224, 224
+    train_data_dir = '../data/train'
+    validation_data_dir = '../data/validation'
+    epochs = 15
+    batch_size = 16  # 每次训练样本数
+
+    # 训练模型
+    train_save_model()
